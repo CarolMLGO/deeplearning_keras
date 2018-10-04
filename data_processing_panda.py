6378eb49ea83d174958895a9722afb2e3863bbbf
Lei_tiger <leifu@oregonstate.edu>
2018-10-03 19:57:18 -0700

    data process1

--- data_processing_panda.py
+++ data_processing_panda.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 # print(pd.__version__)
 pd.set_option('display.expand_frame_repr', False)
-path='./t81_558_deep_learning/data/'
+path='./data/'
 filepath_read=os.path.join(path,'auto-mpg.csv')
 filepath_write=os.path.join(path,'write-out.csv')
 df=pd.read_csv(filepath_read)
