27b705a8d750d0ebe47f5a8534701beb8036639a
Lei_tiger <leifu@oregonstate.edu>
2018-10-03 21:43:56 -0700

    on the way to ML

--- data_processing_panda.py
+++ data_processing_panda.py
@@ -10,9 +10,36 @@ df=pd.read_csv(filepath_read)
 # df.to_csv(filepath_write,index=True)
 # print(df.head())
 
-#perform some basic statistics
-df=df.select_dtypes(include=['int','float'])
-headers=list(df.columns.values)
-print(headers)
+######perform some basic statistics
+# df=df.select_dtypes(include=['int','float'])
+# headers=list(df.columns.values)
+# print(headers)
+
+######Sorting, shuffling dataframes
+np.random.seed(1)
+df=df.reindex(np.random.permutation(df.index))
+# df.reset_index(inplace=True,drop=True)
+
+# mask=np.random.rand(len(df))<0.8
+# train_df=pd.DataFrame(df[mask])
+# val_df=pd.DataFrame(df[~mask])
+# print(train_df[0:10])
+
+# print(f'Trianing set: {len(train_df)}')
+# print(f'Validation set:{len(val_df)}')
+
+#####K-fold validation
+from sklearn.model_selection import KFold
+kf=KFold(5)
+fold=1
+for train_index,validate_index in kf.split(df):
+    train_df=pd.DataFrame(df.iloc[train_index,:])
+    validate_df=pd.DataFrame(df.iloc[validate_index])
+    print(f'Training set: {len(train_df)}; validation set: {len(validate_df)}')
+    fold+=1
+
+
+
+
 
 
--- test.py
+++ test.py
@@ -1,7 +1,37 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-path='./t81_558_deep_learning/data/'
+import numpy as np
+path='./data/'
 filename_read=os.path.join(path,'cgpa.csv')
+
 df=pd.read_csv(filename_read)
-print(df.head())
+data=df.head(100)
+x=data['rollno']
+y=data['cgpa']
+
+fig=plt.figure()
+fig.patch.set_facecolor("gray")
+ax1=fig.add_subplot(1,2,1,facecolor="yellow")
+ax1.plot(x,y)
+
+fig=plt.figure()
+x1=len(data[data.cgpa>=9])
+x2=len(data[(data.cgpa>=8)&(data.cgpa<9)])
+x3=len(data[data.cgpa<8])
+ax2=fig.add_subplot(111)
+ax2.axis('equal')
+ax2.pie([x1,x2,x3],colors=["yellow",'green','red'],labels=['9 pts','8 pts','others'])
+ax2.legend(title="description")
+
+
+
+# plt.plot(data['rollno'],data['cgpa'],color="orange",label="line graph")
+# plt.bar(data['rollno'],data['cgpa'],color=["green","blue","red"])
+# plt.legend()
+# plt.xlabel("Roll No",color="green")
+# plt.ylabel("CGPA",color="blue")
+# plt.title("CGPA VS Roll No",color="green")
+plt.show()
+
+
